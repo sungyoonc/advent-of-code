30b66a2bc4454b4e662e1da71d2ebd906f54acad
Sungyoon Cho <sungyoon@sungyooncho.dev>
2023-08-19 14:58:22 +0900

    feat: 2022 day 04 answer

---
+++ 2022/day04/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string("input.txt").expect("Failed to read line.");
+    // ------- Part 2 -------
+    let pairs_list: Vec<Vec<Vec<i32>>> = data // split string to vectors of int
+        .lines()
+        .map(|x| {
+            x.split(',')
+                .map(|y| {
+                    y.split('-')
+                        .map(|z| z.parse::<i32>().expect("Unable to parse str to i32"))
+                        .collect::<Vec<i32>>()
+                })
+                .collect::<Vec<Vec<i32>>>()
+        })
+        .collect();
+    let mut counter = 0;
+    for pairs in pairs_list.iter() {
+        if !(pairs[0][0] < pairs[1][0] && pairs[0][1] < pairs[1][1]) {
+            if !(pairs[0][0] > pairs[1][0] && pairs[0][1] > pairs[1][1]) {
+                counter += 1;
+            }
+        }
+    }
+    println!("Part 1: {}", counter);
+
+    // ------- Part 2 -------
+    let mut counter = 0;
+    for pairs in pairs_list {
+        if !(pairs[0][1] < pairs[1][0] || pairs[0][0] > pairs[1][1]) {
+            counter += 1;
+        }
+    }
+    println!("Part 2: {}", counter);
+}
